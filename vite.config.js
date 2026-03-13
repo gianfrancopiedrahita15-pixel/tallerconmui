bf2876228e852af2d00c31e82514d379ddbef1cf
shoot <gianfrancopiedrahita15@gmail.com>
2026-03-12 19:02:35 -0500

    Fix base path configuration in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/taller_3_react_mui/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-      base: '/taller_3_react_mui/'
     }),
   ],
 })
