1291bd87fb410e807d1aa14d48753d09ef1dfa6b
gianfrancopiedrahita15-pixel <gianfrancopiedrahita15@gmail.com>
2026-03-12 19:15:03 -0500

    fix: agregar base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   base: '/taller_3_react_mui/',
+=======
+   base: '/taller_3_react_mui/',
+>>>>>>> 0e6cecbd (fix: agregar base para GitHub Pages)
   plugins: [
     react({
       babel: {
