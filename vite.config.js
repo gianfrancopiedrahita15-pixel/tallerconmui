ba03fbee8c9ff8a260cb7afcac7718b3d1d9e256
gianfrancopiedrahita15-pixel <gianfrancopiedrahita15@gmail.com>
2026-03-12 22:12:51 -0500

    fix: remove base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/taller_3_react_mui/',
-  plugins: [
-    react({
+    plugins: [ react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
